8b86e08afb0ad16795ddd09e1e02933d96b9dfc2
SlightlyCyborg <collinalexbell@gmail.com>
2017-08-15 11:16:37 -0400

    added deps to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [ring "1.6.2"]
+                 [http-kit "2.2.0"]
+                 [compojure "1.6.0"]
+                 [hiccup "1.0.5"]
+                 [garden "1.3.2"]
+                 [cheshire "5.7.1"]
+                 [stuarth/clj-oauth2 "0.3.2"]
+                 [org.apache.opennlp/opennlp-tools "1.8.1"]])
