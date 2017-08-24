095698e7a094a873cd94356f49b0020dff06b27c
SlightlyCyborg <collinalexbell@gmail.com>
2017-08-24 08:07:06 -0400

    project.clj: reversion, write description, add url, add license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject elliot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject elliot "1.0.0"
+  :description "A search engine for Elliot's content"
+  :url "http://elliot.daemon.life"
+  :license {:name "The Daemon License"
+            :url "http://daemon.life"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/core.async "0.3.443"]
                  [ring "1.6.2"]
