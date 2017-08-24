b3ef04c227204e6155a045dca5c79ce2a2bfd66f
SlightlyCyborg <collinalexbell@gmail.com>
2017-08-24 08:59:28 -0400

    server.clj -main stops server if already active

--- src/elliot/server.clj
+++ src/elliot/server.clj
@@ -41,5 +41,6 @@
   ;; The #' is useful when you want to hot-reload code
   ;; You may want to take a look: https://github.com/clojure/tools.namespace
   ;; and http://http-kit.org/migration.html#reload
+  (if (not (nil? @server))
+    (stop-server))
   (reset! server (run-server #'app {:port 8080})))
-
