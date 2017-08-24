37bc5a2b100073cd1ca4d731977cdfa975832013
SlightlyCyborg <collinalexbell@gmail.com>
2017-08-24 09:02:41 -0400

    added doc string to stop-server

--- src/elliot/server.clj
+++ src/elliot/server.clj
@@ -30,17 +30,18 @@
 
 (defonce server (atom nil))
 
-(defn stop-server []
+(defn stop-server
+  "Stops server, when an http-kit server named `server` exists"
+  []
   (when-not (nil? @server)
     ;; graceful shutdown: wait 100ms for existing requests to be finished
     ;; :timeout is optional, when no timeout, stop immediately
     (@server :timeout 100)
     (reset! server nil)))
 
-(defn -main [& args]
-  ;; The #' is useful when you want to hot-reload code
-  ;; You may want to take a look: https://github.com/clojure/tools.namespace
-  ;; and http://http-kit.org/migration.html#reload
+(defn -main
+  "Starts or restarts server"
+  [& args]
   (if (not (nil? @server))
     (stop-server))
   (reset! server (run-server #'app {:port 8080})))
