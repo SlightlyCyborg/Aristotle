68993de1e64e598c4db2c22527694eeec47e8017
SlightlyCyborg <collinalexbell@gmail.com>
2017-08-29 08:15:12 -0400

    removed headline cruft

--- src/elliot/templates/home.clj
+++ src/elliot/templates/home.clj
@@ -20,7 +20,6 @@
           [:img {:src "imgs/daemon_img.png" :id "daemon-img"}]]]
         [:div {:class "search-div col"}
          [:div {:class "search-div-contents"}
-          [:h1 (config/all :headline)]
           [:form {:method "GET"}
            [:div {:class "form-group"}
             [:input {:type "text"
