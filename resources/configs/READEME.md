d995fe3ac11f4197f69182ebfa185ae28912d5df
SlightlyCyborg <collinalexbell@gmail.com>
2017-09-01 14:17:52 -0400

    config_loader.clj: loads configs into memory if the config file was updated

--- resources/configs/READEME.md
+++
@@ -1,1 +0,0 @@
-This is where all the config.edns should be stored. Eventually, all of these sites should be config'd using a database, but for now, they should be stored here
--- src/aristotle/config_loader.clj
+++ src/aristotle/config_loader.clj
@@ -2,8 +2,41 @@
   (:require [clojure.edn :as edn]))
 
 
+(defn get-all-config-names []
+  (->>
+   (clojure.java.io/file "resources/configs")
+   .list
+   (into [])))
 
-(defn reload []
-  (def all (edn/read-string (slurp "resources/config.edn"))))
+(defn gen-config-fname [daemon-name]
+  (str "resources/configs/" daemon-name "/config.edn"))
 
-(reload)
+(defn load-config [daemon-name]
+  (println (str "had to slurp file: " daemon-name))
+  (edn/read-string
+   (slurp (gen-config-fname daemon-name))))
+
+(defn get-last-modified [daemon-name]
+  (.lastModified (clojure.java.io/file
+                  (gen-config-fname daemon-name))))
+
+(def all (atom {}))
+
+(defn get [daemon-name]
+  (let [daemon-name (name daemon-name)]
+   (swap! all (fn [last-configs]
+                (merge
+                 last-configs
+                 (let [last-modified (get-last-modified daemon-name)
+                       previous-last-modified (get-in
+                                               last-configs 
+                                               [(keyword daemon-name)
+                                                :last-modified])]
+
+                   (if (or (nil? previous-last-modified)
+                           (> last-modified previous-last-modified))
+                     {(keyword daemon-name)
+                      (assoc (load-config daemon-name)
+                             :last-modified last-modified)}
+                     nil)))))))
+
