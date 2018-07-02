a22b31b3a27ceecd6f2440e521a690deca0d85e0
Collin Bell <collinalexbell@gmail.com>
2018-07-02 06:45:54 +0000

    added bootstrap

--- src/aristotle/templates/home.clj
+++ src/aristotle/templates/home.clj
@@ -10,7 +10,6 @@
      [:html
       [:head
       "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css' integrity='sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M' crossorigin='anonymous'>"
-
       [:style (style/render)]]
      [:body
       [:div {:class "container" :id "search-container"}
