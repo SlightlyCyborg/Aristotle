8588ae74dc0e325e98464f3e2010f3fd4a331817
slightlycyborg <collinalexbell@gmail.com>
2019-04-08 21:31:36 +0000

    updated deps

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/core.async "0.3.443"]
                  [ring "1.6.2"]
                  [clj-http "3.7.0"]
-                 [http-kit "2.3.0"]
+                 [http-kit "2.4.0-alpha3"]
                  [compojure "1.6.0"]
                  [hiccup "1.0.5"]
                  [garden "1.3.2"]
@@ -17,7 +17,7 @@
                  [korma "0.4.0"]
                  [org.xerial/sqlite-jdbc "3.7.15-M1"]
                  [pegasus "0.7.0"]
-                 [clj-solr "1.2"]]
+                 [clj-solr "1.3.0"]]
   :main aristotle.server)
 
 
